eca460df1284f90181bcdc6e02d8b1b5aa46155c
laertt <91387151+laertt@users.noreply.github.com>
2026-05-02 23:56:55 +0200

    Update vite.config.ts

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig(({ command }) => {
   const blogPrerenderRoutes = command === 'build' ? getBlogRoutes() : [];
 
   return {
+     base: '/autoluli/',
     plugins: [
       viteSourceLocator({
         prefix: 'mgx', // Prefix used to identify source locations; do not change.
